c8b7226ca01fcdfc1e7923e9b87cd7ab16423dfb
zafergok <gok.zaferr@gmail.com>
2025-12-09 01:08:06 +0300

    feat: Configure library build with tsup, DTS generation, module exports, and updated peer dependencies.

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
-  dts: false, // DTS build'i tamamen kapatıyoruz
+  dts: true, // DTS build'i etkinleştiriyoruz
   splitting: false,
   sourcemap: true,
   clean: true,
