ebd06db5c8f1603933ef3e33c1186c7e7d55a4b4
zafergok <gok.zaferr@gmail.com>
2025-12-08 18:31:42 +0300

    chore: relax commitlint rules and enhance release-it configuration with git options, hooks, and prompts.

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,9 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [0], // Büyük/küçük harf zorunluluğunu kaldır
+    'subject-full-stop': [0, 'never'], // Nokta ile bitmesine izin ver
+    'header-max-length': [0, 'always', 150], // Başlık uzunluğunu artır
+    'body-max-line-length': [0, 'always', 200], // Gövde satır uzunluğunu artır
+  },
 }
